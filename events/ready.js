d9ffc9ad9613e1e12aa6b15ad75a619a46d6e3b6
LittleWhole <wholelittle@gmail.com>
2018-05-28 00:30:35 -0400

    Buncho fixes

--- events/ready.js
+++ events/ready.js
@@ -1,3 +1,3 @@
 module.exports = client => {
   console.log(`>> Volcanic has started. <<\n=========================\nSTATISTICS\n${client.users.size} users\n${client.channels.size} channels\n${client.guilds.size} servers`);
-});
+};
